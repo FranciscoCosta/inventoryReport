7cfcaf8b5c69a0b111460219038c5dec45dd6c2c
Xico <francisco100eg@gmail.com>
2023-05-08 14:36:16 -0300

    Feat: Test 'cria_produto' function

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_mock = {
+        "id": 1,
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1.",
+    }
+    product = Product(**product_mock)
+
+    for key, value in product_mock.items():
+        assert getattr(product, key) == value
